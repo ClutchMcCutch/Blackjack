2cbfd65774b1ecc5383d4bcc8487a4b21dedd95d
chipsomen <112735090+chipsomen@users.noreply.github.com>
2023-01-28 15:20:51 -0500

    new function wohoo

--- code.js
+++ code.js
@@ -1,11 +1,11 @@
-let dealer = {deck:[], value:0}
+let dealer = {deck:[], value:0, hiddenValue:0}
 let user = {deck:[], value:0}
 let action = {}; // storing functions in an object
 
 user.drawCard = function(x){
     if (!x){x = 1}
     for (let i = 0; i < x; i++){
-        let newCard = randomCard()
+        let newCard = randomCard();
         if (newCard.value == "A"){
             if (user.value + 11 > 21){
                 user.value += 1;
@@ -18,6 +18,41 @@ user.drawCard = function(x){
             user.value += parseInt(newCard.value);
         }
         user.deck.push(newCard);
+    }
+}
+
+dealer.drawCard = function(cards, hiddenCards){
+    if (!cards){cards = 1}
+    if (!hiddenCards){hiddenCards = 0}
+    for (let i = 0; i < cards; i++){
+        let newCard = randomCard();
+        if (newCard.value == "A"){
+            if (dealer.value + 11 > 21){
+                dealer.value += 1;
+            } else {
+                dealer.value += 11;
+            }
+        } else if (isNaN(parseInt(newCard.value)) === true){
+            dealer.value += 10;
+        } else {
+            dealer.value += parseInt(newCard.value);
+        }
+        dealer.deck.push(newCard);
+    }
+    for (let i = 0; i < hiddenCards; i++){
+        let newCard = randomCard(true);
+        if (newCard.value == "A"){
+            if (dealer.value + 11 > 21){
+                dealer.hiddenValue += 1;
+            } else {
+                dealer.hiddenValue += 11;
+            }
+        } else if (isNaN(parseInt(newCard.value)) === true){
+            dealer.hiddenValue += 10;
+        } else {
+            dealer.hiddenValue += parseInt(newCard.value);
+        }
+        dealer.deck.push(newCard);
     }
 }
 
@@ -37,8 +72,7 @@ function randomCard(bool){
 }
 
 function startGame(){
-    dealer.deck.push(randomCard());
-    dealer.deck.push(randomCard(true));
+    dealer.drawCard(1,1);
     user.drawCard(2);
 }
 
@@ -54,4 +88,4 @@ action.stand = function(){}
 action.dd = function(){}
 
 startGame();
-console.log(user)
+console.log(user, dealer)
