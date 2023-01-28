5f3ef193c503d222f55d62cc9c7b1544a63296b4
chipsomen <112735090+chipsomen@users.noreply.github.com>
2023-01-28 17:00:41 -0500

    woohoo

--- code.js
+++ code.js
@@ -56,6 +56,22 @@ dealer.drawCard = function(cards, hiddenCards){
     }
 }
 
+user.checkWin = function(){
+    if (user.value > 21){
+        // lose
+    } else if (user.value == 21){
+        // win
+    }
+}
+
+dealer.checkWin = function(){
+    if (dealer.value > 21){
+        // win
+    } else if (dealer.value == 21){
+        // lose
+    }
+}
+
 function random(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -78,33 +94,23 @@ function startGame(){
 
 action.hit = function(){
     user.drawCard();
-    if (user.value > 21){
-        // lose
-    } else if (user.value == 21){
-        // win
-    }
+    user.checkWin();
 }
 action.stand = function(){
-    dealer.deck[1].hidden = false
     dealer.value += dealer.hiddenValue
     dealer.hiddenValue = 0
-    if (dealer.value > 21){
-        // win
-    } else if (dealer.value == 21){
-        // lose
-    }
+    dealer.deck[1].hidden = false
+    dealer.checkWin();
     while (dealer.value < 17){
         dealer.drawCard();
-        if (dealer.value > 21){
-            // win
-        } else if (dealer.value == 21){
-            // lose
-        }
+        dealer.checkWin();
     }
-    if (dealer.value > 21){
-        // win
-    } else if (dealer.value == 21){
+    if (dealer.value > user.value){
         // lose
+    } else if (dealer.value < user.value){
+        // win
+    } else {
+        // push
     }
 }
 action.dd = function(){}
